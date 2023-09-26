1409dac2a774c5702e1565535ce854fa98d77651
Krasnilov Nikita <n.krasnikov@tg.dunice.net>
2023-09-26 10:45:14 +0300

    examples

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import { bubbleSort, quickSort, mergeSort } from './src';
+import { bubbleSort, quickSort, mergeSort, binarySearch } from './src';
 import { Queue } from './src';
 import { LinkedList } from './src';
 import { Stack } from './src';
@@ -14,6 +14,30 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 const arr = [55,3,999,-7,6,1,9,2,8,3,7,4,6,5,0,0]
-console.log(mergeSort(arr))
-console.log(quickSort(arr))
-console.log(bubbleSort(arr))
+const sorted = quickSort([...arr])
+
+console.log(mergeSort([...arr]))
+console.log(quickSort([...arr]))
+console.log(bubbleSort([...arr]))
+
+console.log('Find! ', binarySearch(sorted, 6))
+
+
+const root = new TreeNode(5)
+root.insert(3)
+root.insert(7)
+root.insert(4)
+root.insert(8)
+root.insert(6)
+root.insert(2)
+root.insert(3)
+root.insert(1)
+root.insert(9)
+root.insert(7)
+
+
+root.travelD()
+console.log('------------')
+root.travelB()
+
+console.log(root.isRoot())
