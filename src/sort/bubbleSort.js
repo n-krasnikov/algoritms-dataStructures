8adf1fb68d63c48758971f82a863d6fa9672849f
Krasnilov Nikita <n.krasnikov@tg.dunice.net>
2023-09-26 10:43:58 +0300

    (sort) change-bubbleSort

--- src/sort/bubbleSort.js
+++ src/sort/bubbleSort.js
@@ -1,10 +1,10 @@
 const bubbleSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[i] < arr[j]) {
-        const tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+    for (let j = 0; j < arr.length-i; j++) {
+      if (arr[j] > arr[j+1]) {
+        const tmp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = tmp;
       };
     };
   };
